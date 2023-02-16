b37fac418509a2e8c8b76e2a0ef076edafc3ae2e
Kerrin Gillis <kerrin.gillis@launchacademy.com>
2023-02-16 10:17:38 -0500

    initial commit

---
+++ server/src/routes/clientRouter.js
@@ -0,0 +1,25 @@
+import express from "express";
+import getClientIndexPath from "../config/getClientIndexPath.js";
+
+const router = new express.Router();
+
+const clientRoutes = ["/", "/user-sessions/new", "/users/new", "/random-page"];
+
+const authedClientRoutes = ["/authed-profile"];
+
+router.get(clientRoutes, (req, res) => {
+  console.log(req.user)
+  res.sendFile(getClientIndexPath());
+});
+
+router.get(authedClientRoutes, (req, res) => {
+  
+  console.log(req.user)
+  if (req.user) {
+    res.sendFile(getClientIndexPath());
+  } else {
+    res.redirect("/user-sessions/new");
+  }
+});
+
+export default router;
